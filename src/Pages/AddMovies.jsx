86c274d4d1167a736c00ff6477d228ff42addc06
Chintia Devi <devi@192.168.1.12>
2022-03-02 13:29:20 +0800

    delete fn HandleSubmit

--- src/Pages/AddMovies.jsx
+++ src/Pages/AddMovies.jsx
@@ -26,25 +26,6 @@ const validateMessages = {
 const AddGames = () => {
   const [user] = useContext(UserContext)
 
-  const handleSubmit = values => {
-    axios.post("https://backendexample.sanbersy.com/api/data-movie", {
-      title: values.title, 
-      genre: values.genre, 
-      duration: values.duration,
-      rating: values.rating,
-      year: values.year,
-      description: values.description,
-      review: values.review,
-      image_url: values.image},
-      {headers: {"Authorization" : `Bearer ${user.token}`}})
-      .then(done => {
-        alert("Succesfull")
-      })
-      .catch((err)=>{
-      alert(err)
-    })
-  }
-
   return (
     <Layout>
        <Link to='/listmovies'><Button style={{margin: "10px 0 15px 25%"}} type="primary"><ArrowLeftOutlined/></Button></Link>
